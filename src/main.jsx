04f26d953aa896b2492086bc725d7c21ee398297
kalyani-377 <bnkalyani3707@gmail.com>
2026-03-03 16:15:19 +0530

    Exhaustive routing fix for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import "./index.css";
 import App from "./App.jsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+// Cosmic initialized
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
